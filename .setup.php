fb33d781599b9f35aa4643b9e764368faed890fb
RHYTHMHACKER <87557923+RHYTHMHACKER@users.noreply.github.com>
2022-03-30 16:25:15 +0530

    Update .setup.php

--- .setup.php
+++ .setup.php
@@ -5,59 +5,59 @@ class set {
     global $system;
     // removing old files
     if ($system=="termux") {
-      system("rm -rf /data/data/com.termux/files/usr/share/IP-Tracer");
-      system("rm -rf /data/data/com.termux/files/usr/bin/ip-tracer");
+      system("rm -rf /data/data/com.termux/files/usr/share/VIP-Tracer");
+      system("rm -rf /data/data/com.termux/files/usr/bin/vip-tracer");
       system("rm -rf /data/data/com.termux/files/usr/bin/trace");
     } elseif ($system=="ubuntu") {
-      system("sudo rm -rf /usr/bin/ip-tracer");
+      system("sudo rm -rf /usr/bin/vip-tracer");
       system("sudo rm -rf /usr/bin/trace");
-      system("sudo rm -rf /usr/share/IP-Tracer");
+      system("sudo rm -rf /usr/share/VIP-Tracer");
     } else {
-      system("rm -rf /usr/bin/ip-tracer");
+      system("rm -rf /usr/bin/vip-tracer");
       system("rm -rf /usr/bin/trace");
-      system("rm -rf /usr/share/IP-Tracer");
+      system("rm -rf /usr/share/VIP-Tracer");
     }
     
     // adding bin file
     if ($system=="termux") {
       system("mv -v modules/ip-tracer /data/data/com.termux/files/usr/bin/");
       system("mv -v modules/trace /data/data/com.termux/files/usr/bin/");
-      system("chmod +x /data/data/com.termux/files/usr/bin/ip-tracer trace");
+      system("chmod +x /data/data/com.termux/files/usr/bin/vip-tracer trace");
       system("chmod +x /data/data/com.termux/files/usr/bin/trace");
     } elseif ($system=="ubuntu") {
-      system("sudo mv -v modules/ip-tracer /usr/bin/");
+      system("sudo mv -v modules/vip-tracer /usr/bin/");
       system("sudo mv -v modules/trace /usr/bin/");
-      system("sudo chmod +x /usr/bin/ip-tracer");
+      system("sudo chmod +x /usr/bin/vip-tracer");
       system("sudo chmod +x /usr/bin/trace");
     } else {
       system("mv -v modules/ip-tracer /usr/bin/");
       system("mv -v modules/trace /usr/bin/");
-      system("chmod +x /usr/bin/ip-tracer");
+      system("chmod +x /usr/bin/vip-tracer");
       system("chmod +x /usr/bin/trace");
     }
 
-    // copy files from IP-Tracer to .IP-Tracer directory.
+    // copy files from VIP-Tracer to .VIP-Tracer directory.
     if ($system=="termux") {
-      system("mkdir /data/data/com.termux/files/usr/share/IP-Tracer");
+      system("mkdir /data/data/com.termux/files/usr/share/VIP-Tracer");
       system("chmod +x * *.* .*.*");
-      system("mv -v * *.* .*.* /data/data/com.termux/files/usr/share/IP-Tracer/");
+      system("mv -v * *.* .*.* /data/data/com.termux/files/usr/share/VIP-Tracer/");
     } elseif ($system=="ubuntu") {
-      system("sudo mkdir /usr/share/IP-Tracer/");
+      system("sudo mkdir /usr/share/VIP-Tracer/");
       system("sudo chmod +x * *.* .*.*");
-      system("sudo mv -v * *.* .*.* /usr/share/IP-Tracer/");
+      system("sudo mv -v * *.* .*.* /usr/share/VIP-Tracer/");
     } else {
-      system("mkdir /usr/share/IP-Tracer");
+      system("mkdir /usr/share/VIP-Tracer");
       system("chmod +x * *.* .*.*");
-      system("mv -v * *.* .*.* /usr/share/IP-Tracer/");
+      system("mv -v * *.* .*.* /usr/share/VIP-Tracer/");
     }
     
-    // removing IP-Tracer directory
+    // removing VIP-Tracer directory
     if ($system=="termux") {
-      system("cd .. && rm -rf IP-Tracer");
+      system("cd .. && rm -rf VIP-Tracer");
     } elseif ($system=="ubuntu") {
-      system("cd .. && sudo rm -rf IP-Tracer");
+      system("cd .. && sudo rm -rf VIP-Tracer");
     } else {
-      system("cd .. && rm -rf IP-Tracer");
+      system("cd .. && rm -rf VIP-Tracer");
     }
   }
   function logo() {
@@ -66,11 +66,13 @@ class set {
 \033[01;33m
 
 
-\033[01;31m      _\033[01;33m ____    _
-     \033[01;31m(_)\033[01;33m  _ \  | |_ _ __ __ _  ___ ___ _ __
-     | | |_) | | __| '__/ _` |/ __/ _ \ '__|
-     | |  __/  | |_| | | (_| | (_|  __/ |
-     |_|_|      \__|_|  \__,_|\___\___|_|
+\033[01;31m      _\033[01;33m 
+╭╮╱╱╭┳━━┳━━━╮╭━━━━╮
+┃╰╮╭╯┣┫┣┫╭━╮┃┃╭╮╭╮┃
+╰╮┃┃╭╯┃┃┃╰━╯┃╰╯┃┃┣┻┳━━┳━━┳━━┳━╮
+╱┃╰╯┃╱┃┃┃╭━┳┻━╮┃┃┃╭┫╭╮┃╭━┫┃━┫╭╯
+╱╰╮╭╯╭┫┣┫┃╱╰━━╯┃┃┃┃┃╭╮┃╰━┫┃━┫┃
+╱╱╰╯╱╰━━┻╯╱╱╱╱╱╰╯╰╯╰╯╰┻━━┻━━┻╯
 
 
     \033[01;37m}\033[01;31m--------------------------------------\033[01;37m{
@@ -80,8 +82,8 @@ class set {
 \033[00m
 EOL;
 
-    if (file_exists("/usr/bin/ip-tracer") || file_exists("/data/data/com.termux/files/usr/bin/ip-tracer")) {
-      echo "\033[01;32m      IP-Tracer installed Successfully !!!\033[00m\n";
+    if (file_exists("/usr/bin/vip-tracer") || file_exists("/data/data/com.termux/files/usr/bin/vip-tracer")) {
+      echo "\033[01;32m      VIP-Tracer installed Successfully !!!\033[00m\n";
       echo <<<EOL
 
 \033[01;37m ----------------------------------------------
@@ -97,7 +99,7 @@ EOL;
 
 EOL;
     } else {
-      echo "\n\n\033[01;31m  Sorry IP-Tracer is not installed !!!\033[00m";
+      echo "\n\n\033[01;31m  Sorry VIP-Tracer is not installed !!!\033[00m";
     }
   }
 }
